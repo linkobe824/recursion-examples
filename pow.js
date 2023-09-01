26804a6692431f39f06cd8382c9fbe8075c51c1a
Manuel Beltran <136205896+linkobe824@users.noreply.github.com>
2023-09-01 11:51:14 -0700

    x power of n - example

---
+++ pow.js
@@ -0,0 +1,8 @@
+const pow = (x, n) => {
+  // caso mas simple
+  if (n === 1) return x;
+  // caso recursivo, hace el problema mas pequeño
+  else return x * pow(x, n - 1);
+};
+
+console.log(pow(2, 3));
