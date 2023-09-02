4ec1d353253e23b776cd9c9fc73e28aab6632add
Manuel Beltran <136205896+linkobe824@users.noreply.github.com>
2023-09-01 18:44:28 -0700

    factorial

--- pow.js
+++ pow.js
@@ -1,8 +1,9 @@
 const pow = (x, n) => {
   // caso mas simple
+  if (n === 0) return 1;
   if (n === 1) return x;
   // caso recursivo, hace el problema mas pequeño
   else return x * pow(x, n - 1);
 };
 
-console.log(pow(2, 3));
+console.log(pow(2, 4));
