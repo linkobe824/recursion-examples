2b3f5ee7f5cfe754f94fd3c0cb1fe88dc07fb7ee
Manuel Beltran <136205896+linkobe824@users.noreply.github.com>
2023-09-01 20:36:45 -0700

    product of array

---
+++ productOfArray.js
@@ -0,0 +1,17 @@
+// Write a function called productOfArray which takes in an
+// array of numbers and returns the product of them all
+
+const productOfArray = (arr) => {
+  if (arr.length === 1) {
+    return arr[0];
+  } else {
+    first = arr[0];
+    arr.shift();
+    return first * productOfArray(arr);
+  }
+};
+
+const six = productOfArray([1, 2, 3]); // 6
+const sixty = productOfArray([1, 2, 3, 10]); // 60
+console.log(six);
+console.log(sixty);
